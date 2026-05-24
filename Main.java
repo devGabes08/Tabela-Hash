8d7bd78c054da2f225549e67465689ab1ac71d97
devGabes08 <dev.gabriel08@gmail.com>
2026-05-24 20:04:03 -0300

    Simplificado a classe main deixando apenas o necessario

--- Main.java
+++ Main.java
@@ -1,60 +1,40 @@
 public class Main {
 
-    static final int CAPACIDADE = 16;
-    static final String ARQUIVO = "nomes_20000_reais_distintos.csv";
-    static final int TOTAL_BUSCAS = 100;
+    public static final int CAPACIDADE = 16;
+    public static final String ARQUIVO = "nomes_20000_reais_distintos.csv";
+    public static final int TOTAL_BUSCAS = 100;
 
     public static void main(String[] args) throws Exception {
-
 
         System.out.println("Lendo arquivo CSV...");
         String[] nomes = LeitorCSV.lerNomes(ARQUIVO);
         System.out.println("Total de nomes lidos: " + nomes.length);
 
-
         TabelaHash t1 = new TabelaHash1(CAPACIDADE);
         TabelaHash t2 = new TabelaHash2(CAPACIDADE);
 
-
         long inicioInsercao1 = System.nanoTime();
-        for (int i = 0; i < nomes.length; i++) {
-            t1.inserir(nomes[i]);
-        }
+        for (int i = 0; i < nomes.length; i++) t1.inserir(nomes[i]);
         long fimInsercao1 = System.nanoTime();
 
-
         long inicioInsercao2 = System.nanoTime();
-        for (int i = 0; i < nomes.length; i++) {
-            t2.inserir(nomes[i]);
-        }
+        for (int i = 0; i < nomes.length; i++) t2.inserir(nomes[i]);
         long fimInsercao2 = System.nanoTime();
 
-
         long inicioBusca1 = System.nanoTime();
-        for (int i = 0; i < TOTAL_BUSCAS && i < nomes.length; i++) {
-            t1.buscar(nomes[i]);
-        }
+        for (int i = 0; i < TOTAL_BUSCAS; i++) t1.buscar(nomes[i]);
         long fimBusca1 = System.nanoTime();
 
         long inicioBusca2 = System.nanoTime();
-        for (int i = 0; i < TOTAL_BUSCAS && i < nomes.length; i++) {
-            t2.buscar(nomes[i]);
-        }
+        for (int i = 0; i < TOTAL_BUSCAS; i++) t2.buscar(nomes[i]);
         long fimBusca2 = System.nanoTime();
 
-
-        imprimirRelatorio(t1, "Hash 1 (Soma ASCII)",
-                fimInsercao1 - inicioInsercao1,
-                fimBusca1 - inicioBusca1);
-
+        imprimirRelatorio(t1, "Hash 1 (Soma ASCII)", fimInsercao1 - inicioInsercao1, fimBusca1 - inicioBusca1);
         System.out.println("\n" + "=".repeat(60) + "\n");
-
-        imprimirRelatorio(t2, "Hash 2 (Polinomial djb2)",
-                fimInsercao2 - inicioInsercao2,
-                fimBusca2 - inicioBusca2);
+        imprimirRelatorio(t2, "Hash 2 (Polinomial djb2)", fimInsercao2 - inicioInsercao2, fimBusca2 - inicioBusca2);
     }
 
-    static void imprimirRelatorio(TabelaHash t, String nome, long tempoIns, long tempoBusca) {
+    public static void imprimirRelatorio(TabelaHash t, String nome, long tempoIns, long tempoBusca) {
         System.out.println("========================================");
         System.out.println("TABELA: " + nome);
         System.out.println("========================================");
@@ -69,29 +49,6 @@ public class Main {
         for (int i = 0; i < tabela.length; i++) {
             String conteudo = tabela[i] != null ? tabela[i] : "(vazio)";
             System.out.printf("Posicao %2d: %s%n", i, conteudo);
-        }
-
-        System.out.println("\n--- Clusterizacao (colisoes por posicao) ---");
-
-        int[] tamanhoCluster = new int[tabela.length];
-        int clusterAtual = 0;
-        int inicioClusters = -1;
-
-        for (int i = 0; i < tabela.length; i++) {
-            if (tabela[i] != null) {
-                if (clusterAtual == 0) inicioClusters = i;
-                clusterAtual++;
-            } else {
-                if (clusterAtual > 0) {
-                    System.out.printf("  Cluster na posicao %2d: tamanho %d (colisoes internas: %d)%n",
-                            inicioClusters, clusterAtual, clusterAtual - 1);
-                }
-                clusterAtual = 0;
-            }
-        }
-        if (clusterAtual > 0) {
-            System.out.printf("  Cluster na posicao %2d: tamanho %d (colisoes internas: %d)%n",
-                    inicioClusters, clusterAtual, clusterAtual - 1);
         }
 
         int ocupadas = 0;
